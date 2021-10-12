8cf47ed4b3eb06f18a175e89322a55874452c17b
tumblxxx <73263453+tumblxxx@users.noreply.github.com>
2021-10-12 17:49:02 +0700

    Create connect-to-blynk-using-esp8266-part1.py

---
+++ connect-to-blynk-using-esp8266-part1.py
@@ -0,0 +1,19 @@
+~ Home$ cd/Users/Home/Desktop/pyserial-2.7 :pyserial-2.7 Home$ sudo python setup.py install
+Password:
+/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/distutils/dist.py:267:
+UserWarning: Unknown distribution option: 'use_2to3'
+ 
+warnings.warn(msg)
+running install
+running build
+running build_py
+running build_scripts
+running install_lib
+running install_scripts
+changing mode of
+/usr/local/bin/miniterm.py to 755
+running install_egg_info
+Removing /Library/Python/2.7/site-packages/pyserial-2.7-py2.7.egg-info
+Writing
+/Library/Python/2.7/site-packages/pyserial-2.7-py2.7.egg-info
+:pyserial-2.7 Home$
